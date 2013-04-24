31745d39e7186445d8c4f4e13bd97fd735a0a398
Larry Davis <lazd@lazd.net>
2013-04-24 06:09:49 -0700

    Change multi-task name for benchmark

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function(grunt) {
 			tasks: ['jshint', 'test']
 		},
 		benchmark: {
-			singleTest: {
+			all: {
 				src: ['benchmarks/*.js'],
 				dest: 'build/testResults.csv'
 			}
